fbf9ee348090c3ef25b1d429075baf14b9b3213c
mujtaba-mac <mujtabaali60333@gmail.com>
2026-02-24 13:01:46 +0530

    fixed infinite loop by updating loop

--- whileloop.py
+++ whileloop.py
@@ -1,9 +1,28 @@
-k2=[10,20,30,40,50]
+count=2
+while count<18: #infinite loop because you don'nt include steps to increment
+    age=int(input("Enter your age:"))
+    count=20
+print("success : i am out of loop")    
+   
+    
+
+
+'''numbers=["ali","mujtaba",1,8,9,78]
+i = 0
+while i<len(numbers):
+    print(numbers)
+    i+=1'''
+
+
+
+
+
+'''k2=[10,20,30,40,50]
 x=0
 l=len(k2)
 while x<l:
     print(x)
-    x=x+1
+    x=x+1'''
 
 
 '''k2=[10,20,30,40,50]
